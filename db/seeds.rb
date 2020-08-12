e71593be1fc357d93afc1006efcac43c7b089194
Aniket-1906 <aniket@progton.com>
2020-08-12 12:58:05 +0530

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post#{blog}" ,
+		body: "Since the beginning of the internet, millions and millions and millions of blogs have been created. Many have died due to lost interest or their owners giving up on the idea, while others have thrived and continue to grow, making money and earning their owners a steady income. It’s a constant evolution of content that keeps people coming back for more, especially if these blogs contact highly resourceful material that people find useful and interesting."
+		)
+
+end
+
+puts "10 blog post created"
+
+5.times do |skill|
+	Skill.create!(
+			title: "Rails #{skill}",
+			percent_utilized: 15
+		)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+			title: "Portfolio title: #{portfolio_item}",
+			subtitle: "My great service",
+			body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+			main_image: "https://via.placeholder.com/600x400",
+			thumb_image: "https://via.placeholder.com/350x200",
+		)
+end
+
+puts "9 portfolio items created"
